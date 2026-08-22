e5b9508c057027771f2e93fd778089f0ed9c71ea
Sonam Sahu <sonamsahu3101@gmail.com>
2026-08-22 23:41:43 +0530

    Add files via upload

---
+++ PrintSum.java
@@ -0,0 +1,12 @@
+public class PrintSum {
+    public static void main(String[] args){
+        int arr[] = {10, 20, 30};
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+
+        }
+         System.out.println("Sum of array elements is: " + sum);
+
+    }
+}
