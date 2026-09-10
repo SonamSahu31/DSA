edc65bd574c36498c7499e454e733386ace7a69a
Sonam Sahu <sonamsahu3101@gmail.com>
2026-09-11 00:15:50 +0530

    Add files via upload

---
+++ Positive_Negative.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Positive_Negative {
+    public static void main(String[] args){
+        // Scanner class is used to take input from the user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        if(num==0){
+            System.out.println("The number is zero");
+        
+        }
+        else if(num>0){
+            System.out.println("The number is positive");
+            
+        }
+        else{
+            System.out.println("The number is negative");
+        }
+        
+    }
+    
+}
+ 
