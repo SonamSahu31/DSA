4f2a341e36f1c099b03d89cc4df3d77bd14b5c77
Sonam Sahu <sonamsahu3101@gmail.com>
2026-09-16 20:54:11 +0530

    Add files via upload

---
+++ SolidRectage.java
@@ -0,0 +1,16 @@
+package pattern;
+
+public class SolidRectage {
+    public static void main(String[] args){
+        int n = 3; //Number of rows
+        int m = 5; //Number of columns
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+
+    }
+    
+}
